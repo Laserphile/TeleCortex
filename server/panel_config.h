015283e08b224cfd078a751b66f6379ea636bf96
derwentx <derwentx@gmail.com>
2018-02-22 11:34:52 +1100

    update config for new testing board

--- server/panel_config.h
+++ server/panel_config.h
@@ -1,18 +1,20 @@
-#if defined(__AVR_ATmega328P__) // Derwent's testing setup on Arduino Uno
+#define STATUS_PIN 13
+
+#if defined(__AVR_ATmega328P__) || defined(__MK20DX128__) // Derwent's testing setup on Arduino Uno
     #define PANEL_TYPE NEOPIXEL
     #define NEEDS_CLK 0
     #define PANEL_00_DATA_PIN 6
     #define PANEL_00_CLK_PIN 0
-    #define PANEL_00_LEN 100
+    #define PANEL_00_LEN 333
     #define PANEL_01_DATA_PIN 7
     #define PANEL_01_CLK_PIN 0
-    #define PANEL_01_LEN 90
-    #define PANEL_02_DATA_PIN 0
+    #define PANEL_01_LEN 260
+    #define PANEL_02_DATA_PIN 8
     #define PANEL_02_CLK_PIN 0
-    #define PANEL_02_LEN 100
-    #define PANEL_03_DATA_PIN 0
+    #define PANEL_02_LEN 333
+    #define PANEL_03_DATA_PIN 9
     #define PANEL_03_CLK_PIN 0
-    #define PANEL_03_LEN 100
+    #define PANEL_03_LEN 333
     #define MAX_PANELS 4
 #else // Matt's Live Setup on Teensy 3.2
     #define PANEL_TYPE APA102
