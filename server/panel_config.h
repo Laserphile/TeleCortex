2e03a40b2f7b6d3f878d4e0cd913e42735507862
derwentx <derwentx@gmail.com>
2018-03-15 07:38:52 +0800

    swap position of large panel

--- server/panel_config.h
+++ server/panel_config.h
@@ -6,13 +6,13 @@
     #define NEEDS_CLK 0
     #define PANEL_00_DATA_PIN 6
     #define PANEL_00_CLK_PIN 0
-    #define PANEL_00_LEN 316
+    #define PANEL_00_LEN 260
     #define PANEL_01_DATA_PIN 7
     #define PANEL_01_CLK_PIN 0
     #define PANEL_01_LEN 260
     #define PANEL_02_DATA_PIN 8
     #define PANEL_02_CLK_PIN 0
-    #define PANEL_02_LEN 260
+    #define PANEL_02_LEN 316
     #define PANEL_03_DATA_PIN 9
     #define PANEL_03_CLK_PIN 0
     #define PANEL_03_LEN 260
@@ -23,13 +23,13 @@
     #define NEEDS_CLK 1
     #define PANEL_00_DATA_PIN 7
     #define PANEL_00_CLK_PIN 13
-    #define PANEL_00_LEN 316
+    #define PANEL_00_LEN 260
     #define PANEL_01_DATA_PIN 7
     #define PANEL_01_CLK_PIN 14
     #define PANEL_01_LEN 260
     #define PANEL_02_DATA_PIN 11
     #define PANEL_02_CLK_PIN 13
-    #define PANEL_02_LEN 260
+    #define PANEL_02_LEN 316
     #define PANEL_03_DATA_PIN 11
     #define PANEL_03_CLK_PIN 14
     #define PANEL_03_LEN 260
