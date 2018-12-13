d54222bd1e90f3a02d8bfe042e392ccaa3651df1
derwentx <derwentx@gmail.com>
2018-12-14 10:37:55 +1100

    remove max command size, checksum checks, and waits, 30FPS all channels

--- server/config.h
+++ server/config.h
@@ -15,27 +15,29 @@
 #define DEBUG_EEPROM 0
 
 /* Command processing flags */
-#define REQUIRE_CHECKSUM 1
-#define REQUIRE_CONSECUTIVE_LINENUM 1
+#define REQUIRE_CHECKSUM 0
+#define REQUIRE_CONSECUTIVE_LINENUM 0
+#define REPLY_OK 0
 
 /* Display rainbows until receive GCode */
 #define RAINBOWS_UNTIL_GCODE 1
+#define RAINBOWS_ON_IDLE 0
 #define ENABLE_GAMMA_CORRECTION 1
 #define MAX_BRIGHTNESS 85
 #define EEPROM_SETTINGS 1
 
-#define APA_DATA_RATE 1
+#define APA_DATA_RATE 10
 
 // Maximum command length
-#define MAX_CMD_SIZE 260
+#define MAX_CMD_SIZE 1500
 
 // Number of commands in the queue
 #define MAX_QUEUE_LEN 10
 
 // TIMING
 #define LOOP_WAIT_PERIOD 0
-#define LOOP_IDLE_PERIOD 100
-#define LOOP_DEBUG_PERIOD 1000
+#define LOOP_IDLE_PERIOD 0
+#define LOOP_DEBUG_PERIOD 100
 #define FAIL_WAIT_PERIOD 0
 
 #define DISABLE_QUEUE 0
@@ -51,4 +53,3 @@
 #include "board_properties.h"
 
 #endif /* __CONFIG_H__ */
-
